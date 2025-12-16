8dc4a8706d9323f0fe48868bc6de0745c8b2f4c3
Pratik garad <pratikgarad1221@gmail.com>
2025-12-16 21:45:54 +0530

    update

---
+++ blogs/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    Category_name = models.CharField(max_length=50, unique=True)
+    Created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name_plural="categories"
+
+    
+    def __str__(self):
+        return self.Category_name
+
+
+STATUS_CHOICES=(
+    ("Draft", "Draft"),
+    ("Published", "Published")
+     
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150,unique=True,blank=True)
+    Category =models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=2000)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES, default="Draf")
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+    
+
+
