44090d1177ecbe772ada9b06154002e4da89040f
Pratik garad <pratikgarad1221@gmail.com>
2025-12-28 11:42:51 +0530

    Add Funcnality

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,5 +1,12 @@
 from .models import Category
+from Core_features.models import Sociallinks
 
 def get_categories(request):
     categories = Category.objects.all()
     return {'categories': categories}
+
+
+def get_sociallinks(request):
+     
+    sociallinks = Sociallinks.objects.all()
+    return dict(sociallinks=sociallinks)
