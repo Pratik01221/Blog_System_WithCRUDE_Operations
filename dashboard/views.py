8cdf41e19a2d5c015d1270a7c66e6ba010dae934
Pratik garad <pratikgarad1221@gmail.com>
2026-01-17 15:01:53 +0530

    update category

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
+from .forms import CategoryForm
+
 
 # Create your views here.
 
@@ -16,3 +18,38 @@ def dashboards(request):
         'blogs_count': blogs_count,
     }   
     return render(request, 'Dashboards/dashboard.html', context)
+
+
+
+def categories(request):
+    return render(request, 'Dashboards/categories.html')
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+
+    form = CategoryForm()
+    context= {
+        'form': form,
+        }
+
+    return render(request, 'Dashboards/add_category.html', context) 
+
+
+def edit_category(request, pk):
+    category= get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form= CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form= CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category,
+        }
+    return render(request, 'Dashboards/edit_category.html', context)
